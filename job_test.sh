7d3a1e17ad9a13b997460ce8150544edbd97ac3c
Andrea Crespi <144369740+drew2799@users.noreply.github.com>
2024-05-28 20:58:50 +0200

    Update job_test.sh

--- job_test.sh
+++ job_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-#SBATCH --time=00:10:00
+
+#SBATCH --job-name=test1
+#SBATCH --output=out_test1.out
+#SBATCH --time=15:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=1024M
+
+module load julia/1.10.0
+
+srun julia --project=/home/acrespi/FILEBAIN /home/acrespi/FILEBAIN/test1.jl
